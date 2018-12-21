6afe35815881ba59f1b71aee2b11863569548ef8
Baron Desmond <baron@desmond.com>
2018-12-21 09:41:37 -0600

    t

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,40 +1,51 @@
-import React, { Component } from 'react';
-import { TouchableOpacity, View, ActivityIndicator, Text, Alert} from 'react-native';
-export default class LinkScreen extends Component {
-handlePress = async () => {
-  fetch('https://data.advance88.hasura-app.io/v1/query', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-          "type": "select",
-    "args": {
-        "table": "author",
-        "columns": [
-            "name"
-        ],
-        "limit": "1"
-    }
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
+
+export default class LinkSreen extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
       })
-})
-    .then((response) => response.json())
-    .then((responseJson) => {
- Alert.alert("Author name at 0th index:  " + responseJson[0].name);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
   render(){
-  return(
-   <View style={{paddingTop: 50, paddingLeft: 50 }}>
-   <Text> Some other text </Text>
-    <Text> Some other text </Text>
-    <TouchableOpacity onPress={this.handlePress.bind(this)}>
-     <Text style={{paddingTop: 50, paddingLeft: 50, color: '#FF0000'}}> Click me to see the name </Text>
-    </TouchableOpacity>
-</View> 
-  );
-}
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
+      </View>
+    );
+  }
 }
