1935fafcb60b40b518705e759643c1423044dd85
Baron Desmond <baron@desmond.com>
2019-02-04 14:03:19 -0600

    t

--- constants/common.js
+++
@@ -1,8 +0,0 @@
-export const COLOR_PRIMARY = '#58C9B9';
-export const COLOR_SECONDARY = '#111';
-export const FONT_NORMAL = 'OpenSans-Regular';
-export const FONT_BOLD = 'OpenSans-Bold';
-export const BORDER_RADIUS = 5;
-export const URL = 'https://app.plisolutions.com:7443/primelogic/';
-export const STORAGE_KEY = '@MySuperStore:key';
-export const AUTHKEY = 'xxx';
