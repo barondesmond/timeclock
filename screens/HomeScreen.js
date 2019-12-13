7881ef6c91e667958849d43bc20dc15d15ecb0cf
barondesmond <baron@desmond.com>
2019-12-13 13:21:14 -0600

    t

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -355,8 +355,7 @@ render()
           </View>
 	  
       {this.renderDevice()}
-	  {this.renderPortal('Job')}
-	  {this.renderPortal('Dispatch')}
+
 	  {this.renderPortal('Employee')}
 	  {this.renderPortal('Document')}
 	  {__DEV__ ? <Button title="Logout" onPress={this.resetKeys} /> : <Button title="Logout" onPress={this.resetKeys} />}
